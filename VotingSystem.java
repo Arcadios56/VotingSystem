c6f79d7c0a5ac07b6250c23453be02a549221b74
Aracdios56 <akinmi044@gmail.com>
2025-09-03 21:36:50 +0100

    Add files via upload

--- VotingSystem.java
+++ VotingSystem.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class VotingSystem {
@@ -6,13 +5,17 @@ public class VotingSystem {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter number of candidates: ");
         int candidatesNumber = scanner.nextInt();
-        String[] candidates = new String[candidatesNumber];
+        scanner.nextLine(); 
+        
+String[] candidates = new String[candidatesNumber];
         int[] votes = new int[candidatesNumber];
 
-
-String [] Candidates = new String[candidatesNumber];
-
+        for (int index = 0; index < candidatesNumber; index++) {
+            System.out.print("Enter candidate " + (index + 1) + " name: ");
+            candidates[index] = scanner.nextLine();
 }
-  }
 
-
+        System.out.print("Enter number of voters: ");
+        int votersNumber = scanner.nextInt();
+}
+}
